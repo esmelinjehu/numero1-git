8c9083ebeb53e72c1d7428922449d11f65c8004c
esmelinjehu <carlh_12@hotmail.com>
2017-06-08 16:49:16 -0500

    añadiendo todo el proyecto

---
+++ src/com/pe/proyecto/modelo/Alumno.java
@@ -0,0 +1,26 @@
+package com.pe.proyecto.modelo;
+
+public class Alumno {
+ private String nombre;
+ private int dni;
+ 
+ public Alumno() {
+	// TODO Auto-generated constructor stub
+}
+ 
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public int getDni() {
+	return dni;
+}
+public void setDni(int dni) {
+	this.dni = dni;
+}
+ 
+	
+	
+}
